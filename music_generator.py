1bac6f3f6c293776c7ab788d91e6e6d7d31bf4c0
Nicholas Beerbower <nbeerbower@gmail.com>
2023-11-19 19:33:47 -0500

    compress to ogg

--- music_generator.py
+++ music_generator.py
@@ -1,13 +1,13 @@
-print("Loading audiocraft...")
 from audiocraft.models import MusicGen
-print("Loading torchaudio...")
 import torchaudio
+from pydub import AudioSegment
 
 class MusicGenerator:
-    def __init__(self, model_name='facebook/musicgen-small'):
-      # Initialize MusicGen with a pre-trained model
-      print("Loading model ", model_name, "...")
-      self.model = MusicGen.get_pretrained(model_name)
+    def __init__(self, model_name='facebook/musicgen-small', compress_audio=True):
+        # Initialize MusicGen with a pre-trained model
+        print("Loading model ", model_name, "...")
+        self.model = MusicGen.get_pretrained(model_name)
+        self.compress_audio = compress_audio
 
     def set_params(self, use_sampling=True, top_k=250, duration=15):
         # Set generation parameters
@@ -29,3 +29,13 @@ class MusicGenerator:
         output_cpu = output[0].cpu()
         torchaudio.save(file_name, output_cpu, sample_rate=32000)
         print(f"Audio generated and saved as {file_name}")
+
+        if self.compress_audio:
+            self._compress_audio(file_name)
+
+    def _compress_audio(self, file_name):
+        # Compress the audio to ogg
+        print("Compressing audio...")
+        sound = AudioSegment.from_wav(file_name)
+        compress_file_name = file_name.replace(".wav", ".ogg")
+        sound.export(compress_file_name, format="ogg", codec="libvorbis")
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, send_file
 from music_generator import MusicGenerator  # Assuming this is your class
 
 app = Flask(__name__)
-music_gen = MusicGenerator(model_name='facebook/musicgen-stereo-medium')
+music_gen = MusicGenerator(model_name='facebook/musicgen-stereo-medium', compress_audio=True)
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -11,11 +11,11 @@ def generate():
     duration = data.get('duration')
     if duration:
             music_gen.set_params(duration=duration)
-    file_name = "output_audio.wav"
-    music_gen.generate_music(prompt, file_name)
+    file_name = "output_audio.ogg"
+    music_gen.generate_music(prompt)
     return send_file(file_name, as_attachment=True)
 
 if __name__ == '__main__':
     port = 5000
     print("Starting server on port", port)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)

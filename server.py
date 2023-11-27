528c76b5b0c744e2672d63a6eaa598b067bf5f6f
Nicholas Beerbower <nbeerbower@gmail.com>
2023-11-27 14:52:47 -0500

    better error and default param handling

--- server.py
+++ server.py
@@ -13,6 +13,13 @@ model_name = os.getenv('UTAPY_MODEL', 'facebook/musicgen-stereo-medium')
 music_gen = MusicGenerator(model_name=model_name, compress_audio=True)
 task_queue = queue.Queue()
 
+class Task:
+    def __init__(self, prompt, params, event):
+        self.prompt = prompt
+        self.params = params
+        self.event = event
+        self.error = None
+
 def worker():
     """
     Worker thread to process music generation tasks.
@@ -22,14 +29,14 @@ def worker():
         if task is None:
             break
 
-        prompt, params, event = task
         try:
-            music_gen.set_params(**params)
-            music_gen.generate_music(prompt)
+            music_gen.set_params(**task.params)
+            music_gen.generate_music(task.prompt)
         except Exception as e:
             logging.error(f"Error in generating music: {e}")
+            task.error = e
         finally:
-            event.set()
+            task.event.set()
             task_queue.task_done()
 
 # Start worker thread
@@ -46,19 +53,27 @@ def generate():
         
         # Extract additional parameters with defaults
         params = {
-            'duration': data.get('duration', 30.0),
-            'use_sampling': data.get('use_sampling', True),
-            'top_k': data.get('top_k', 250),
-            'top_p': data.get('top_p', 0.0),
-            'temperature': data.get('temperature', 1.0),
-            'cfg_coef': data.get('cfg_coef', 3.0),
-            'two_step_cfg': data.get('two_step_cfg', False),
-            'extend_stride': data.get('extend_stride', 18)
+            'duration': 30.0,
+            'use_sampling': True,
+            'top_k': 250,
+            'top_p': 0.0,
+            'temperature': 1.0,
+            'cfg_coef': 3.0,
+            'two_step_cfg': False,
+            'extend_stride': 18
         }
+        for key in params:
+            if key in data and data[key] is not None:
+                params[key] = data[key]
+        logging.info(f"Received request to generate music from prompt: {prompt}")
 
         event = threading.Event()
-        task_queue.put((prompt, params, event))
+        task = Task(prompt, params, event)
+        task_queue.put(task)
         event.wait()
+        
+        if task.error is not None:
+            return "Internal Server Error: music generation failed", 500
 
         # Ensure the file exists
         if not os.path.exists("output_audio.ogg"):
@@ -67,7 +82,7 @@ def generate():
         return send_file("output_audio.ogg", as_attachment=True)
     except Exception as e:
         logging.error(f"Error in generate route: {e}")
-        return "Internal Server Error", 500
+        return "Internal Server Error: handling request failed", 500
 
 if __name__ == '__main__':
     port = int(os.getenv('UTAPY_PORT', 5002))  # Use environment variable for port, default to 5002

91ab9dcd3f20c4805d2f104281c44d0b4b3bf481
Nicholas Beerbower <nbeerbower@gmail.com>
2023-11-27 12:19:29 -0500

    pep8 for music_gen class

--- music_generator.py
+++ music_generator.py
@@ -1,16 +1,26 @@
+import logging
 from audiocraft.models import MusicGen
 import torchaudio
 from pydub import AudioSegment
 
+
 class MusicGenerator:
+    """
+    Music generator class using audiocraft.models.MusicGen for generating music from text prompts.
+    """
+
     def __init__(self, model_name='facebook/musicgen-small', compress_audio=True):
-        # Initialize MusicGen with a pre-trained model
-        print("Loading model ", model_name, "...")
+        """
+        Initialize the MusicGen model with a pre-trained model and set compression settings.
+        """
+        logging.info(f"Loading model {model_name}...")
         self.model = MusicGen.get_pretrained(model_name)
         self.compress_audio = compress_audio
 
     def set_params(self, use_sampling=True, top_k=250, duration=15):
-        # Set generation parameters
+        """
+        Set generation parameters for the music generation model.
+        """
         self.model.set_generation_params(
             use_sampling=use_sampling,
             top_k=top_k,
@@ -18,24 +28,40 @@ class MusicGenerator:
         )
 
     def generate_music(self, prompt, file_name="output_audio.wav"):
-        # Generate music with a text prompt
-        print("Generating music from prompt: ", prompt, "...")
-        output = self.model.generate(
-            descriptions=[prompt],
-            progress=True
-        )
+        """
+        Generate music based on the given text prompt and save the audio to a file.
+        """
+        logging.info(f"Generating music from prompt: {prompt}...")
+        try:
+            output = self.model.generate(
+                descriptions=[prompt],
+                progress=True
+            )
 
-        # Move the tensor to CPU and then save the generated audio to a file
-        output_cpu = output[0].cpu()
-        torchaudio.save(file_name, output_cpu, sample_rate=32000)
-        print(f"Audio generated and saved as {file_name}")
+            output_cpu = output[0].cpu()
+            torchaudio.save(file_name, output_cpu, sample_rate=32000)
+            logging.info(f"Audio generated and saved as {file_name}")
 
-        if self.compress_audio:
-            self._compress_audio(file_name)
+            if self.compress_audio:
+                self._compress_audio(file_name)
+
+        except Exception as e:
+            logging.error(f"Error in generating music: {e}")
 
     def _compress_audio(self, file_name):
-        # Compress the audio to ogg
-        print("Compressing audio...")
-        sound = AudioSegment.from_wav(file_name)
-        compress_file_name = file_name.replace(".wav", ".ogg")
-        sound.export(compress_file_name, format="ogg", codec="libvorbis")
+        """
+        Compress the generated audio file to ogg format.
+        """
+        logging.info("Compressing audio...")
+        try:
+            sound = AudioSegment.from_wav(file_name)
+            compress_file_name = file_name.replace(".wav", ".ogg")
+            sound.export(compress_file_name, format="ogg", codec="libvorbis")
+            logging.info(f"Audio compressed and saved as {compress_file_name}")
+
+        except Exception as e:
+            logging.error(f"Error in compressing audio: {e}")
+
+
+# Setup basic logging
+logging.basicConfig(level=logging.INFO)
